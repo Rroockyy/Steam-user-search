f30887639629f77f63a4492d35d19546b1d23301
Rocky <kawa2009b@gmail.com>
2026-02-13 23:58:00 +0100

    added user input + show all users data from the first page

--- main.py
+++ main.py
@@ -26,17 +26,44 @@ def wait_and_scrape(url, css_selector):
     # Get the page source and parse with BeautifulSoup
     soup = BeautifulSoup(driver.page_source, 'html.parser')
     
-    # Now you can use BeautifulSoup methods to extract data
-    # For example:
-    # data = soup.select(css_selector)
-    
     return soup
 
 # Usage
-username = "majkel"
+
+print("Enter username to search: ")
+username = input()
+print("Enter real name to search (optional): ")
+realName = input()
+print("Enter location to search (optional): ")
+location = input()
+print("Does the person have a profile picture? y/n (optional): ")
+pfpPresent = input()
+
 steamURL = "https://steamcommunity.com/search/users/#page=1&text=" + username
 css_selector = ".search_row"
 soup = wait_and_scrape(steamURL, css_selector)
 mydivs = soup.find_all("div", {"class": "search_row"})
 for user in mydivs:
-    print(user.find("a", {"class": "searchPersonaName"}).text)
+    userSiblings = []
+    for element in user.find("a", {"class": "searchPersonaName"}).next_siblings:
+        userSiblings.append(element)
+
+    currentUsername = user.find("a", {"class": "searchPersonaName"}).text
+
+    currentRealName = userSiblings[1].strip()
+    if len(userSiblings) == 4 and userSiblings[-1] == ' ':
+        currentRealName = ""
+
+    currentLocation = ""
+    if userSiblings[-1] == ' ':
+        currentLocation = userSiblings[-3].strip()
+
+    currentHasPfp = True
+    if user.find("img")['src'] == "https://avatars.fastly.steamstatic.com/fef49e7fa7e1997310d705b2a6158ff8dc1cdfeb_medium.jpg":
+        currentHasPfp = False
+        
+    print(currentUsername)
+    print(currentRealName)
+    print(currentLocation)
+    print(currentHasPfp)
+    print()
